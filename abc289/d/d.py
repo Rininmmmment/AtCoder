c8991b4684e6de10116afa519677a4374c9504c3
Rininmmmment <rinr10693otes@gmail.com>
2023-02-12 03:25:27 +0900

    setsetsetset!!!!
    hoge in listはもうやめます

--- abc289/d/d.py
+++ abc289/d/d.py
@@ -1,26 +1,22 @@
+# hige in listはクソ遅い。setにすると速くなる！
+
 N = int(input())
 A = list(map(int, input().split()))
 M = int(input())
-B = list(map(int, input().split()))
+B = set(list(map(int,input().split())))
 X = int(input())
 
 dp = [None] * (X+1) # i段目に登れればTrue
 dp[0] = True
 
-for i in range(M):
-    dp[B[i]] = False
-
 for i in range(1, X+1):
     for j in range(N):
         if i in B:
-            dp[i] = False
+            continue
         elif i >= A[j]:
             if dp[i-A[j]]:
                 dp[i] = True
-        else:
-            dp[i] = False
 
-print(dp)
 if dp[X]:
     print("Yes")
 else:
