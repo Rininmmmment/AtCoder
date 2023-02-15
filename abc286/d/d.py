6554964497dcf9fc33ae76fb1abec8809786106f
Rininmmmment <rinr10693otes@gmail.com>
2023-02-15 14:19:30 +0900

    add
    abc286 d AC

--- abc286/d/d.py
+++ abc286/d/d.py
@@ -1,42 +1,26 @@
 N, X = map(int, input().split())
-A, B = [None] * N, [None] * N
+A, B = [], []
 for i in range(N):
-    A[i], B[i] = map(int, input().split())
-
-C = []
-for i in range(N):
-    for j in range(B[i]):
-        C.append(A[i])
-
-# i枚の効果で和をjにできるか
-dp = [[None] * (X + 1) for i in range(len(C)+1)]
+    ai, bi = map(int, input().split())
+    A.append(ai)
+    B.append(bi)
+    
+dp = [[False for _ in range(X + 1)] for _ in range(N + 1)]
 dp[0][0] = True
-for j in range(1, X+1): # 0枚の時は全てFalse
-	dp[0][j] = False
+for j in range(1, X+1):
+    dp[0][j] = False
 
-for i in range(1, len(C)+1):
-	for j in range(0,X+1):
-		if j < C[i-1]:
-            # カードiなしですでに和がjならTrue
-			if dp[i-1][j] == True:
-				dp[i][j] = True
-            # そうでなければA[i-1]を足してもjにならないのでFalse
-			else:
-				dp[i][j] = False
-
-		if j >= C[i-1]:
-            # すでに和がj、もしくはカードi-1までで和がj-A[i-1]ならTrue
-			if dp[i-1][j] == True or dp[i-1][j-C[i-1]] == True:
-				dp[i][j] = True
-			else:
-				dp[i][j] = False
-# 出力
-ans = False
-for i in range(N):
-    if dp[i][X] == True:
-        ans = True
+for i in range(1, N + 1):
+    for j in range(X + 1):
+        if dp[i-1][j]:
+            dp[i][j] = True
+        for k in range(1, B[i-1]+1):
+            index = j-k*A[i-1]
+            if 0 <= index:
+                if dp[i-1][index] == True:
+                    dp[i][j] = True
 
 if dp[N][X]:
-	print("Yes")
+    print("Yes")
 else:
-	print("No")
+    print("No")
